56147f5eb279b1cfc148cea82c42ff56257c7cd8
ellentaylor827 <101396715+ellentaylor827@users.noreply.github.com>
2022-03-11 14:03:42 +0000

    Add files via upload
    allows text in a text file to be read and analysed; need to add the function to allow you to input text

---
+++ AnalyseText.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class AnalyseText
+{
+
+	public void countSentences(string textToAnalyse)
+	{
+		string search = ".";
+		int count = 0;
+		for (int i = 0; i < textToAnalyse.Length; i++)
+		{
+			if (textToAnalyse[i] == char.Parse(search))
+			{
+				count++;
+
+			}
+		}
+		Console.WriteLine("Amount of sentences: " + count);
+
+	}
+
+	public void countVowels(string textToAnalyse)
+	{
+		int vowelsCount = 0;
+		string textLower = textToAnalyse.ToLower();
+
+		string[] vowels = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U"};
+		for (int i = 0; i < textToAnalyse.Length; i++)
+        {
+			foreach (string letter in vowels)
+			{
+				char letterToCompare = char.Parse(letter);
+				if (letterToCompare == textToAnalyse[i])
+				{
+					vowelsCount++;
+				}
+
+			}
+        }
+		Console.WriteLine("Amount of vowels: " + vowelsCount);
+	}
+
+	public void countConsonants(string textToAnalyse)
+    {
+		int consonantsCount = 0;
+		string[] vowels = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+
+		for (int i = 0; i < textToAnalyse.Length; i++)
+		{
+			bool isVowel = false;
+			foreach (string letter in vowels)
+			{
+				if (textToAnalyse[i] == char.Parse(letter))
+				{
+					isVowel = true;
+					break;
+				}
+			}
+
+			if (Char.IsLetter(textToAnalyse[i]) && isVowel == false)
+			{
+				consonantsCount++;
+			}
+		}
+		Console.WriteLine("Amount of consonants: " + consonantsCount);
+	}
+
+
+	public void countUpper(string textToAnalyse)
+	{
+		int upperCount = 0;
+		for (int i = 0; i < textToAnalyse.Length; i++)
+        {
+			bool isUpper = Char.IsUpper(textToAnalyse[i]);
+			if (isUpper == true)
+            {
+				upperCount++;
+            }
+
+		}
+		Console.WriteLine("Amount of uppercase: " + upperCount);
+	}
+
+	public void countLower(string textToAnalyse)
+	{
+		int lowerCount = 0;
+		for (int i = 0; i < textToAnalyse.Length; i++)
+		{
+			bool isLower = Char.IsLower(textToAnalyse[i]);
+			if (isLower == true)
+			{
+				lowerCount++;
+			}
+
+		}
+		Console.WriteLine("Amount of lowercase: " + lowerCount);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Local list of integers to hold the first five measurements of the text
+            List<int> parameters = new List<int>();
+
+            //Create 'Input' object
+            //Get either manually entered text, or text from a file
+            string enterOrRead;
+            Console.WriteLine("do you want to enter text via the keyboard (enter), or read from a text file(read): ");
+            enterOrRead = Console.ReadLine();
+            if (enterOrRead == "enter")
+            {
+                Console.WriteLine("enter text");
+            }
+            else if (enterOrRead == "read")
+            {
+                Console.WriteLine("read text");
+                string text = File.ReadAllText(@"C:\Users\ellen\Documents\Uni\OOP_assessment1\readFile.txt");
+                Console.WriteLine(text);
+                string textToAnalyse = text;
+
+                AnalyseText analyseAll = new AnalyseText();
+                analyseAll.countSentences(textToAnalyse);
+                analyseAll.countVowels(textToAnalyse);
+                analyseAll.countConsonants(textToAnalyse);
+                analyseAll.countUpper(textToAnalyse);
+                analyseAll.countLower(textToAnalyse);
+
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+            //TO ADD: Get the frequency of individual letters?
+
+
+        }
+
+
+
+    }
+}
